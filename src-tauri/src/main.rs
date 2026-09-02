b2de35fad413a946c2e3eb697aa166bce377eeaf
不会游泳的鱼 <1451778027@qq.com>
2026-09-02 14:35:42 +0800

    add rust command foundation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,4 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    wsl_dev_center_lib::run()
-}
+fn main() { wsl_dev_center_lib::run_app() }
